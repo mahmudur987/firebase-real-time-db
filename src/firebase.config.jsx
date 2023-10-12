7b9b2294e1a0c57141ad9704195af2415c02be16
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2023-10-12 12:03:06 +0600

    Update firebase.config.jsx

--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
@@ -10,16 +9,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_appId,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB1os-qi8hvg3s1yqSsH6kODzPnbpm1vtU",
-//   authDomain: "fir-realtime-db-8491a.firebaseapp.com",
-//   projectId: "fir-realtime-db-8491a",
-//   storageBucket: "fir-realtime-db-8491a.appspot.com",
-//   messagingSenderId: "548939423048",
-//   appId: "1:548939423048:web:57ad1afa980cda1106a77d",
-// };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
