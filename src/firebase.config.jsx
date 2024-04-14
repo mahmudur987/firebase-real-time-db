38b93b2c4d10eeb86601b98ebac42b3ce75525cc
mahmudur987 <safemahmud987@agmail.com>
2024-04-15 03:01:31 +0600

    Update firebase.config.jsx

--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_apiKey,
   authDomain: import.meta.env.VITE_APP_authDomain,
